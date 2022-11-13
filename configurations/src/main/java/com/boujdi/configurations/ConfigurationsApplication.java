3ef6c6d1d7d78135b1007712adf4ea3052bbf05b
mohamed boujdi <boujdi.medz@gmail.com>
2022-11-13 23:17:55 +0100

    add k/v secrets tovault using VaultTamplete

--- configurations/src/main/java/com/boujdi/configurations/ConfigurationsApplication.java
+++ configurations/src/main/java/com/boujdi/configurations/ConfigurationsApplication.java
@@ -1,13 +1,36 @@
 package com.boujdi.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.Versioned;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class ConfigurationsApplication {
-
+	@Autowired
+  private VaultTemplate vaultTemplate;
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationsApplication.class, args);
+	}
+
+	/*@Bean
+	public String getSecret() {
+		return vaultTemplate.read("secret/mysecret").getData().get("password").toString();
+	}*/
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Versioned.Metadata put = vaultTemplate
+					.opsForVersionedKeyValue("secret")
+					.put("kaypair", Map.of("password", "123456", "username", "boujdi", "email", "boujdii.me@gmail.com"));
+
+		};
 	}
 
 }
